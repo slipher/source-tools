c6ac847a33356a1e5c21c7e7b6783a4ad5bfee88
slipher <slipher@protonmail.com>
2024-05-30 14:22:46 -0500

    Rework check_pch_include

--- check_pch_include.py
+++ check_pch_include.py
@@ -3,24 +3,30 @@
 import os, os.path
 import re
 
-SRC = 'C:/unv/unvanquished/daemon/src'
+def check(src, header):
+    header = header.encode('ascii')
+    for dirpath, _, filenames in os.walk(src):
+        if dirpath.endswith('skeletons'): continue
+        for f in filenames:
+            if not f.endswith('.cpp'): continue
+            fname = os.path.join(dirpath, f)
+            includes = [re.search(br'["<](.*)[">]', l).group(1)
+                        .split(b'/')[-1]
+                        for l in open(fname, 'rb')
+                        if re.match(br'\s*#\s*include.*["<]', l)]
+            icommon = [i for i,l in enumerate(includes) if l == header]
 
+            if not icommon:
+                problem = 'Not included'
+            elif len(icommon) > 1:
+                problem = 'Multiply included'
+            elif icommon[0] != 0:
+                problem = 'Not first'
+            else:
+                problem = None
+            if problem:
+                print('%-25s %s' % (problem, fname))
 
-for dirpath, _, filenames in os.walk(SRC):
-    for f in filenames:
-        f = f.lower()
-        if not f.endswith('.cpp'): continue
-        fname = os.path.join(dirpath, f)
-        includes = [l.lower() for l in open(fname) if '#include' in l]
-        icommon = [i for i,l in enumerate(includes) if re.search(r'\bcommon\.h', l)]
-
-        if not icommon:
-            problem = 'Not included'
-        elif len(icommon) > 1:
-            problem = 'Multiply included'
-        elif icommon[0] != 0:
-            problem = 'Not first'
-        else:
-            problem = None
-        if problem:
-            print '%-25s %s' % (problem, fname)
+check('C:/unv/Unvanquished/daemon/src', 'Common.h')
+print()
+check('C:/unv/Unvanquished/src', 'Common.h')
